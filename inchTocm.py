2ddb6c44a29ba4a1fe6ecaab23be47ee725241bf
shk0505 <shk05year@gmail.com>
2025-03-19 16:02:29 +0900

    파이썬프로그래밍 2주차 과제

---
+++ inchTocm.py
@@ -0,0 +1,2 @@
+Ans = float(input("inch 단위 길이를 입력해주세요 : "))
+print(f"{Ans}inch는 {Ans*2.54}cm입니다.")
---
+++ kgToPound.py
@@ -0,0 +1,2 @@
+Ans = float(input("킬로그램 단위 무게를 입력해주세요 : "))
+print(f"{Ans}kg는 {Ans*2.205}파운드입니다.")
